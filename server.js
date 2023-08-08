310f2788437bdf1d728e6eda4c54fe83eb08cea8
EmersonLaranja <emersonlaranja@gmail..com>
2023-08-08 16:35:41 -0300

    1.3 - tsconfig +verificação rigorosa + watch

--- server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-var app_js_1 = require("./src/app.js");
-var port = process.env.PORT || 3000;
-app_js_1.default.listen(port, function () {
-    console.log("Servidor escutando em http://localhost:".concat(port));
-});
