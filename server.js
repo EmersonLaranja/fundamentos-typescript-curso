50dd42b4ae311c74a52452ce79be918ad6fdc22f
EmersonLaranja <emersonlaranja@gmail..com>
2023-08-08 14:49:10 -0300

    configuração inicial: estrutura de pastas + express + dotenv + gitignore

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ import app from "./src/app.js";
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
 });
