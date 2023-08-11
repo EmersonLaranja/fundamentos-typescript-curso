6f5e171337520e382d5042b8a1595a2fe5656560
EmersonLaranja <emersonlaranja@gmail.com>
2023-08-11 11:11:58 -0300

    3.1

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import router from "./routes/index.js"; // precisa da extensão .js por ser EcmaSCript
-
+import "reflect-metadata"
 
 const app = express();
 app.use(express.json());
