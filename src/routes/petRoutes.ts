d62522a28a3d60dff9f7ee40690f8ebd7e92ce97
EmersonLaranja <emersonlaranja@gmail.com>
2023-08-11 09:03:17 -0300

    2.2

--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -4,6 +4,9 @@ const router = express.Router();
 
 router.post("/",PetController.criaPet);
 router.get("/",PetController.listaPets);
+router.get("/",PetController.listaPetPelaEspecie);
+router.put("/",PetController.atualizaPet);
+router.delete("/",PetController.deletaPet);
 
 
 export default router; 
